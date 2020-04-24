b3a899d655f961a4668ef4f26399a181d8f2465e
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2020-04-24 21:07:09 +0200

    List of games returned

--- functions/getAllGames.js
+++ functions/getAllGames.js
@@ -2,15 +2,13 @@ exports.handler = function(event, context, callback) {
     return callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          data:  {
-            id: 1,
-            title: 'Test Game',
-            description: 'Dit is een omschrijving van het spel',
-            duration: 0,
-            preparationTime: 0,
-            age: 0,
-            material: []
-          }
+          id: 1,
+          title: 'Test Game',
+          description: 'Dit is een omschrijving van het spel',
+          duration: 0,
+          preparationTime: 0,
+          age: 0,
+          material: []
         })
       })
 }
