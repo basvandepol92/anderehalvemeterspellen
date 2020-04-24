1df08a44fb21fda6ea7c2d39273296870b866bab
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2020-04-24 21:13:23 +0200

    Added netlify _redirects

--- functions/getAllGames.js
+++ functions/getAllGames.js
@@ -1,14 +1,18 @@
-exports.handler = function(event, context, callback) {
-    return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          id: 1,
-          title: 'Test Game',
-          description: 'Dit is een omschrijving van het spel',
-          duration: 0,
-          preparationTime: 0,
-          age: 0,
-          material: []
-        })
-      })
-}
+exports.handler = function (event, context, callback) {
+  const listOfGames = [
+    {
+      id: 1,
+      title: "Test Game",
+      description: "Dit is een omschrijving van het spel",
+      duration: 0,
+      preparationTime: 0,
+      age: 0,
+      material: [],
+    },
+  ];
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(listOfGames),
+  });
+};
