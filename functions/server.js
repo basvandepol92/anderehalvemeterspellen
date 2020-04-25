d140d776777346afbd0f4c4e5b2ff1de9eb4c237
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2020-04-25 21:29:32 +0200

    Added logging for netlify function

--- functions/server.js
+++ functions/server.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 
-const dbUrl = process.env.DB_URL,
-      dbOptions = {
-        useNewUrlParser: true,
-        useFindAndModify: false
-      }
+const dbUrl = process.env.DB_URL;
+console.log(dbUrl); 
+const dbOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+};
 // Set DB from mongoose connection
-mongoose.connect(dbUrl, dbOptions)
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+mongoose.connect(dbUrl, dbOptions);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 module.exports.db = db;
